6c1f55a856ad02c32f9080ea44b75295b15399ce
sksiva726 <sksivakumar726@gmail.com>
2025-07-01 19:21:39 +0530

    3rd commit calculator2

--- calculator.py
+++ calculator.py
@@ -9,3 +9,10 @@ def calculator1(a, b):
     divide = a / b if b != 0 else 'undefined'
     return add, multiply, divide
 
+# 3rd commit
+def calculator2(a, b):
+    add = a + b
+    multiply = a * b
+    divide = a / b if b != 0 else 'undefined'
+    subtract = a - b
+    return add, multiply, divide, subtract
