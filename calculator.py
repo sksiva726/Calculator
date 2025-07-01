f7c71cfdb7374d4b3927a4694617e475db8f74df
sksiva726 <sksivakumar726@gmail.com>
2025-07-01 19:17:29 +0530

    2nd commit adnd calculator1

--- calculator.py
+++ calculator.py
@@ -1,3 +1,11 @@
 # simple calculator for testing the git versions
 def calculator(a, b):
     return a + b, a * b
+
+# second commit
+def calculator1(a, b):
+    add = a + b
+    multiply = a * b
+    divide = a / b if b != 0 else 'undefined'
+    return add, multiply, divide
+
